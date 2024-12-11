5f730f7bef124f5fa0ee45a52cc8e05c7b3398cb
gabirez <99507312+gabirez@users.noreply.github.com>
2024-12-10 22:25:38 -0800

    Add files via upload

---
+++ Aim 2: Diversity Analysis/Unweighted UniFrac PCoA Plots/PCoA Unweighted Unifrac Visualizations (FINAL).R
@@ -0,0 +1,73 @@
+library(tidyverse)
+library(phyloseq)
+library(vegan)
+library(ggokabeito)
+
+load("ps_rare_renamed.RData")
+samp_data_wdiv <- data.frame(sample_data(ps_rare), estimate_richness(ps_rare))
+
+#### Beta diversity analysis #####
+
+#calculate weighted unifrac distance matrix
+unifrac <- UniFrac(ps_rare, weighted = FALSE)
+
+#Perform principal coordinates analysis (PCoA) on the unifrac distance matrix.
+pcoa_unifrac <- ordinate(ps_rare, method="PCoA", distance = "unifrac", weighted = FALSE)
+
+#Reorder factor levels in Stage_FusoAbundance to make legend follow biological disease progression
+ps_rare@sam_data$Group <- factor(ps_rare@sam_data$Group, levels = c("Healthy control (HC)", "Chronic gastritis (CG)", "Intestinal metaplasia (IM）",  "Intraepithelial neoplasia (IN)", "Gastric cancer (GC)"))
+
+#### Fusobacterium ####
+
+#plot PCoA with ellipses + significant differences
+
+gg_pcoa_fuso_simple <- gg_pcoa_fuso_simple <- plot_ordination(ps_rare, pcoa_unifrac, color = "Group", shape = "Fusobacterium_abundance") +
+                      stat_ellipse(type = "norm", level = 0.95, aes(group = Group)) +
+                      scale_color_okabe_ito(
+                        labels = c(
+                          "Healthy control (HC)" = "Healthy Control",
+                          "Chronic gastritis (CG)" = "Chronic Gastritis",
+                          "Intestinal metaplasia (IM）" = "Intestinal Metaplasia",
+                          "Intraepithelial neoplasia (IN)" = "Intraepithelial Neoplasia",
+                          "Gastric cancer (GC)" = "Gastric Cancer")) + 
+                      guides(color = guide_legend(title = "Disease stage"), shape = guide_legend(title = expression(italic("Fusobacterium") ~ "abundance"))) +
+                      theme_test()
+
+gg_pcoa_fuso_simple
+
+#run PERMANOVA 
+adonis2(unifrac ~ Stage_FusoAbundance, data = samp_data_wdiv, permutations = 999)
+
+
+#Save visualization file.
+ggsave("gg_pcoa_fuso_simple.png"
+       , gg_pcoa_fuso_simple
+       , height=4, width=8)
+
+#### H. pylori ####
+
+gg_pcoa_hpyl_simple <- gg_pcoa_fuso_simple <- plot_ordination(ps_rare, pcoa_unifrac, color = "Group", shape = "H.pylori_Status") +
+                        stat_ellipse(type = "norm", level = 0.95, aes(group = Group)) +
+                        scale_color_okabe_ito(
+                          labels = c(
+                            "Healthy control (HC)" = "Healthy Control",
+                            "Chronic gastritis (CG)" = "Chronic Gastritis",
+                            "Intestinal metaplasia (IM）" = "Intestinal Metaplasia",
+                            "Intraepithelial neoplasia (IN)" = "Intraepithelial Neoplasia",
+                            "Gastric cancer (GC)" = "Gastric Cancer")) + 
+                        guides(color = guide_legend(title = "Disease stage"), shape = guide_legend(title = expression(italic("H. pylori") ~ "status"))) +
+                        theme_test()
+
+gg_pcoa_hpyl_simple
+
+#run PERMANOVA 
+adonis2(unifrac ~ Stage_Pylori, data = samp_data_wdiv, permutations = 999)
+
+#Save visualization file.
+ggsave("gg_pcoa_hpyl_simple.png"
+       , gg_pcoa_hpyl_simple
+       , height=4, width=7.5)
+
+#run PERMANOVA based on disease stage 
+adonis2(unifrac ~ Group, data = samp_data_wdiv, permutations = 999)
+
---
+++ Aim 2: Diversity Analysis/Unweighted UniFrac PCoA Plots/phyloseqobjectcode_renamed.R
@@ -0,0 +1,76 @@
+#### Install Packages ####
+
+library(phyloseq)
+library(ape) # importing trees
+library(tidyverse)
+library(vegan)
+
+#### Load Data ####
+
+#load metadata
+metafp <- "LH_gastric_cancer_metadata_new.tsv"
+meta <- read_delim(metafp, delim="\t")
+#load feature table
+otufp <- "table.txt"
+otu <- read_delim(file = otufp, delim="\t", skip=1) 
+#load taxonomy data
+taxfp <- "taxonomy.tsv"
+tax <- read_delim(taxfp, delim="\t")
+#load phylogenetic tree
+phylotreefp <- "tree.nwk"
+phylotree <- read.tree(phylotreefp)
+
+#### Format Feature Table ####
+
+#save everything except first column (OTU ID) into a matrix
+otu_mat <- as.matrix(otu[,-1])
+# Make first column (#OTU ID) the rownames of the new matrix
+rownames(otu_mat) <- otu$`#OTU ID`
+# Use the "otu_table" function to make an OTU table
+OTU <- otu_table(otu_mat, taxa_are_rows = TRUE) 
+class(OTU)
+
+#### Format Sample Metadata ####
+
+#Save everything except sampleid as new data frame
+samp_df <- as.data.frame(meta[,-1])
+# Make sample ids the rownames
+rownames(samp_df)<- meta$'sample-id'
+# Make phyloseq sample data with sample_data() function
+SAMP <- sample_data(samp_df)
+class(SAMP)
+
+#### Format Taxonomy Data ####
+
+# Convert taxon strings to a table with separate taxa rank columns
+tax_mat <- tax %>% select(-Confidence)%>%
+  separate(col=Taxon, sep="; "
+           , into = c("Domain","Phylum","Class","Order","Family","Genus","Species")) %>%
+  as.matrix() # Saving as a matrix
+# Save everything except feature IDs 
+tax_mat <- tax_mat[,-1]
+# Make sample ids the rownames
+rownames(tax_mat) <- tax$`Feature ID`
+# Make taxa table
+TAX <- tax_table(tax_mat)
+class(TAX)
+
+#### Create Phyloseq Object ####
+
+# Merge all into a phyloseq object
+ps <- phyloseq(OTU, SAMP, TAX, phylotree)
+
+#### Filter Data ####
+
+# Remove non-bacterial sequences, if any
+ps_filt <- subset_taxa(ps,  Domain == "d__Bacteria" & Class!="c__Chloroplast" & Family !="f__Mitochondria")
+
+#### Rarefy samples ####
+rarecurve(t(as.data.frame(otu_table(ps_filt))), cex=0.1)
+ps_rare <- rarefy_even_depth(ps_filt, rngseed = 1, sample.size = 7381)
+
+#### Save Data ####
+
+save(ps_filt, file="ps_filt.RData")
+save(ps_rare, file="ps_rare_renamed.RData")
+
